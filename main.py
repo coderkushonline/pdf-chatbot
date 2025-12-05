2c39ddac7459634095d4013766558d96abc68215
coderkush <codeai424@gmail.com>
2025-12-05 18:55:28 +0545

    FastAPI integration

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 from conversation import GetPdfChatBot
 
-chatbot = GetPdfChatBot(filepath="mypdf.pdf")
+from fastapi import FastAPI
+from pydantic import BaseModel
 
-while True:
-    uip = input("Enter your query: ")
-    if uip.lower == "exit":
-        break
-    print(chatbot.qna(uip))
-        
+class Model(BaseModel):
+    filepath: str
+    query: str
+    descriptsion: str | None = None
+
+
+app = FastAPI()
+
+@app.post('/ask_ai')
+def home(data: Model):
+    try:
+        chatbot = GetPdfChatBot(filepath=data.filepath)
+        response = chatbot.qna(data.query)
+        return {"content": response, "status": 200}
+    
+    except Exception as e:
+        return {"content": "An error occured while connecting with the LLM. Please try again later", "status": 400}
+
